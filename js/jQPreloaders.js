7c15b807bf848398dc7aeb573264216d3b69670b
Fede91 <user@emailserver.fake>
2014-01-11 14:04:52 +0100

    Add the project structure

---
+++ js/jQPreloaders.js
@@ -0,0 +1,10 @@
+/*
+* jQPreloaders.js
+* Author: Fede91
+* Github: https://github.com/Fede91
+*
+* Made available under a MIT License:
+* http://www.opensource.org/licenses/mit-license.php
+*
+* jQPreloaders.js Copyright Fede91 2014.
+*/
